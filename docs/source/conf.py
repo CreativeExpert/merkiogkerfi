41aa7671931d36c73f81f1467b4fc63adaea9a20
eaa96 <100771830+eaa96@users.noreply.github.com>
2022-03-05 16:51:00 +0000

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,4 +1,7 @@
 import sphinx_rtd_theme
+import sys
+import os
+import re
 
 html_theme = "sphinx_rtd_theme"
 html_theme_path = ["_themes", ]
@@ -11,7 +14,7 @@ html_theme_options = {
     'prev_next_buttons_location': 'bottom',
     'style_external_links': False,
     'vcs_pageview_mode': '',
-    'style_nav_header_background': 'blue',
+    'style_nav_header_background': 'light',
     
     # Toc options
     'collapse_navigation': True,
@@ -25,9 +28,9 @@ html_theme_options = {
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Merki og kerfi (RAF301G)'
+copyright = '2022, Einar Ari Árnason'
+author = 'Einar Ari Árnason'
 
 release = '0.1'
 version = '0.1.0'
@@ -59,112 +62,3 @@ epub_show_urls = 'footnote'
 
 
 # -*- coding: utf-8 -*-
-
-import sys
-import os
-import re
-
-# Prefer to use the version of the theme in this repo
-# and not the installed version of the theme.
-sys.path.insert(0, os.path.abspath('..'))
-sys.path.append(os.path.abspath('./demo/'))
-
-import sphinx_rtd_theme
-from sphinx_rtd_theme import __version__ as theme_version
-from sphinx_rtd_theme import __version_full__ as theme_version_full
-from sphinx.locale import _
-
-project = u'Read the Docs Sphinx Theme'
-slug = re.sub(r'\W+', '-', project.lower())
-version = theme_version
-release = theme_version_full
-author = u'Dave Snider, Read the Docs, Inc. & contributors'
-copyright = author
-language = 'en'
-
-extensions = [
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.mathjax',
-    'sphinx.ext.viewcode',
-    'sphinxcontrib.httpdomain',
-    'sphinx_rtd_theme',
-]
-
-templates_path = ['_templates']
-source_suffix = '.rst'
-exclude_patterns = []
-locale_dirs = ['locale/']
-gettext_compact = False
-
-master_doc = 'index'
-suppress_warnings = ['image.nonlocal_uri']
-pygments_style = 'default'
-
-intersphinx_mapping = {
-    'rtd': ('https://docs.readthedocs.io/en/stable/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
-}
-
-html_theme = 'sphinx_rtd_theme'
-html_theme_options = {
-    'logo_only': True,
-    'navigation_depth': 5,
-}
-html_context = {}
-
-if not 'READTHEDOCS' in os.environ:
-    html_static_path = ['_static/']
-    html_js_files = ['debug.js']
-
-    # Add fake versions for local QA of the menu
-    html_context['test_versions'] = list(map(
-        lambda x: str(x / 10),
-        range(1, 100)
-    ))
-
-html_logo = "demo/static/logo-wordmark-light.svg"
-html_show_sourcelink = True
-
-htmlhelp_basename = slug
-
-
-latex_documents = [
-  ('index', '{0}.tex'.format(slug), project, author, 'manual'),
-]
-
-man_pages = [
-    ('index', slug, project, [author], 1)
-]
-
-texinfo_documents = [
-  ('index', slug, project, author, slug, project, 'Miscellaneous'),
-]
-
-
-# Extensions to theme docs
-def setup(app):
-    from sphinx.domains.python import PyField
-    from sphinx.util.docfields import Field
-
-    app.add_object_type(
-        'confval',
-        'confval',
-        objname='configuration value',
-        indextemplate='pair: %s; configuration value',
-        doc_field_types=[
-            PyField(
-                'type',
-                label=_('Type'),
-                has_arg=False,
-                names=('type',),
-                bodyrolename='class'
-            ),
-            Field(
-                'default',
-                label=_('Default'),
-                has_arg=False,
-                names=('default',),
-            ),
-        ]
-    )
