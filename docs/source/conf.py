1f2671655bb5ddf619eef1558c21febf332423f1
eaa96 <100771830+eaa96@users.noreply.github.com>
2022-03-05 15:44:01 +0000

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@ html_theme_options = {
     
     # Toc options
     'collapse_navigation': True,
-    'sticky_navigation': True,
+    'sticky_navigation': False,
     'navigation_depth': 4,
     'includehidden': True,
     'titles_only': False
